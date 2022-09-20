0a4ce8db04a0021b0f58759426545e53db19688c
hmash-1821 <plaguekilla301@gmail.com>
2022-09-20 15:44:45 +0400

    first commit

---
+++ day1/lesson1.py
@@ -0,0 +1,29 @@
+#name = "temo"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჯებელი სიმბოლო
+# "temo" არის ცვალდისთვის მნიშვნელობა
+#surname = "maghlakelidze"
+
+
+#print(name)
+
+#print(surname)
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "temo" # ეს არის str (string) ტიპის ცვლადი
+surname = "maghlakeldze"
+age = 14 # ეს არის int (integer)მთელი რიცხვი
+height = 180.0 #ეს არის float ტიპის ცვლადი
+weight = 70
+#boolean (bool) ტიპის ცვლად
+knows_programming = "True"
+
+studying_programming = "vswavlob programirebas"
+
+
+#homework ერთ ხაზზში გამოვიტანოთ ჩვენი პირადი ინფორმაცია
+
+
+
+
+print("me mqvia" + " " + name + " " + surname + " " + "var" + " " + str(age) + "" + "wlis" +  " " + "simagle" + str(height) +" " + "wona" + " " + str(weight) + " " + "kilogrami" + " " + studying_programming)
