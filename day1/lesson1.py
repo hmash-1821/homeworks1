1ee9e3b222ecda4329ec20f93399db4136cfe819
hmash-1821 <plaguekilla301@gmail.com>
2022-09-21 14:28:02 +0400

    second commit

--- day1/lesson1.py
+++ day1/lesson1.py
@@ -16,14 +16,27 @@ age = 14 # ეს არის int (integer)მთელი რიცხვი
 height = 180.0 #ეს არის float ტიპის ცვლადი
 weight = 70
 #boolean (bool) ტიპის ცვლად
-knows_programming = "True"
+
+#studying_programming = "True"
+
 
 studying_programming = "vswavlob programirebas"
-
+is_londonscammer  = True
 
 #homework ერთ ხაზზში გამოვიტანოთ ჩვენი პირადი ინფორმაცია
 
 
-
+#first hard way
 
 print("me mqvia" + " " + name + " " + surname + " " + "var" + " " + str(age) + "" + "wlis" +  " " + "simagle" + str(height) +" " + "wona" + " " + str(weight) + " " + "kilogrami" + " " + studying_programming)
+
+
+#second easier way
+
+everything = "me mqvia temo maghlakelidze var 14 wlis simagle 180.0 wona 70 kilogrami vswavlob programirebas."
+
+print(everything)
+
+
+
+
